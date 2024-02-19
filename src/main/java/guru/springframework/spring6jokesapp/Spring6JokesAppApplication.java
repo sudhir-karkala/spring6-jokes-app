ef34ea0f7b81292092be102675c53324989c2452
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-02-19 20:10:22 +0530

    create spring boot project, closes #1.

---
+++ src/main/java/guru/springframework/spring6jokesapp/Spring6JokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring6jokesapp/Spring6JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
